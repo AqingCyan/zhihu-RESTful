f1beaafdfd40644a653787a16c976457ed98b82d
AqingCyan <aqingcyan@cyanthing.com>
2020-05-31 21:18:34 +0800

    Parameter calibration

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -6,6 +6,9 @@ class UsersCtl {
   }
 
   findById(ctx) {
+    if (ctx.params.id * 1 >= db.length) {
+      ctx.throw(412)
+    }
     ctx.body = db[ctx.params.id * 1]
   }
 
@@ -19,11 +22,21 @@ class UsersCtl {
   }
 
   update(ctx) {
+    if (ctx.params.id * 1 >= db.length) {
+      ctx.throw(412)
+    }
+    ctx.verifyParams({
+      name: { type: 'string', required: true },
+      age: { type: 'number', required: false },
+    })
     db[ctx.params.id * 1] = ctx.request.body
     ctx.body = ctx.request.body
   }
 
   delete(ctx) {
+    if (ctx.params.id * 1 >= db.length) {
+      ctx.throw(412)
+    }
     db.splice(ctx.params.id * 1, 1)
     ctx.status = 204
   }
