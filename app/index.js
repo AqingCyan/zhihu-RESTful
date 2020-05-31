590ef85d053ccd1c9b9324041b37781a0da22528
AqingCyan <aqingcyan@cyanthing.com>
2020-05-31 21:14:48 +0800

    Parameter calibration

--- app/index.js
+++ app/index.js
@@ -1,34 +1,25 @@
 const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
 const error = require('koa-json-error')
+const parameter = require('koa-parameter')
 const routing = require('./routes')
 
 const app = new Koa()
 
 /**
  * Error handling with koa-json-error:
- * {
- *   "stack": "NotFoundError: Not Found\n at Object.throw xxxxxxx",
- *   "message": "Not Found",
- *   "name": "NotFoundError",
- *   "status": 404
- * }
- * but no stack call information is returned in production
- * {
- *   "message": "Not Found",
- *   "name": "NotFoundError",
- *   "status": 404
- * }
+ * But no stack call information is returned in production
+ * Detail in README
  */
-app.use(error({
+const errHandle = {
   postFormat: (err, { stack, ...rest }) => (
     process.env.NODE_ENV === 'production' ? rest : { stack, ...rest }
   )
-}))
+}
 
+app.use(error(errHandle))
 app.use(bodyparser())
-
-// Batch registration route
-routing(app)
+app.use(parameter(app)) // Parameter calibration
+routing(app) // Batch registration route
 
 app.listen(3000, () => { console.log('app is running on port 3000') })
