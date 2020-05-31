f30326a37fb1324ce49612112871b4b42662824e
AqingCyan <aqingcyan@cyanthing.com>
2020-05-31 23:24:56 +0800

    Collate annotations and access MongoDB Atlas

--- app/index.js
+++ app/index.js
@@ -2,9 +2,15 @@ const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
 const error = require('koa-json-error')
 const parameter = require('koa-parameter')
+const mongoose = require('mongoose')
 const routing = require('./routes')
+const { connectionStr } = require('../config')
 
 const app = new Koa()
+
+// Connect MongoDB Atlas
+mongoose.connect(connectionStr, { useNewUrlParser: true }, () => console.log('MongoDB connect success'))
+mongoose.connection.on('error', console.error)
 
 /**
  * Error handling with koa-json-error:
@@ -17,9 +23,16 @@ const errHandle = {
   )
 }
 
+/**
+ * The middlewares used by Koa
+ * koa-json-error：Error handling and response to JSON
+ * koa-bodyparser：Parsing the Body
+ * koa-parameter：Parameter calibration
+ * Batch registration route
+ */
 app.use(error(errHandle))
 app.use(bodyparser())
-app.use(parameter(app)) // Parameter calibration
-routing(app) // Batch registration route
+app.use(parameter(app))
+routing(app)
 
 app.listen(3000, () => { console.log('app is running on port 3000') })
---
+++ config/README.md
@@ -0,0 +1,11 @@
+# 安全性解释
+
+文件配置涉及到个人隐私问题，这里不上传真实 config 下的 `index.js`
+
+学习该项目请自行在该目录下创建 `index.js`，内容如下：
+
+```js
+module.exports = {
+  connectionStr: '你的 MongoDB Atlas 链接 code'
+}
+```
